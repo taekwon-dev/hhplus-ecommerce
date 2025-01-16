930f30cd7f8c13bd5c9a55f7a8f8d14331fe8d25
taekwon-dev <devtkwon@gmail.com>
2025-01-16 15:01:07 +0900

    [FEAT] HTTP 요청/응답 로깅 처리

--- src/main/java/kr/hhplus/be/server/config/web/WebMvcConfig.java
+++ src/main/java/kr/hhplus/be/server/config/web/WebMvcConfig.java
@@ -3,7 +3,10 @@ package kr.hhplus.be.server.config.web;
 import kr.hhplus.be.server.domain.user.service.UserService;
 import kr.hhplus.be.server.global.auth.AuthInterceptor;
 import kr.hhplus.be.server.global.auth.UserArgumentResolver;
+import kr.hhplus.be.server.global.filter.HttpLoggingFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -16,6 +19,14 @@ import java.util.List;
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private final UserService userService;
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new HttpLoggingFilter());
+        filterRegistrationBean.addUrlPatterns("/v1/**");
+        return filterRegistrationBean;
+    }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
