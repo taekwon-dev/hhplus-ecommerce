fd982871183174f58935437867266194f918bca1
taekwon-dev <devtkwon@gmail.com>
2025-01-16 23:32:28 +0900

    [TEST] PaymentService 포인트 잔액 부족 케이스 테스트

--- src/test/java/kr/hhplus/be/server/integration/domain/payment/service/PaymentServiceTest.java
+++ src/test/java/kr/hhplus/be/server/integration/domain/payment/service/PaymentServiceTest.java
@@ -7,6 +7,7 @@ import kr.hhplus.be.server.domain.payment.domain.PaymentMethod;
 import kr.hhplus.be.server.domain.payment.domain.PaymentStatus;
 import kr.hhplus.be.server.domain.payment.service.PaymentService;
 import kr.hhplus.be.server.domain.point.domain.Point;
+import kr.hhplus.be.server.domain.point.exception.InsufficientPointBalanceException;
 import kr.hhplus.be.server.domain.point.repository.PointRepository;
 import kr.hhplus.be.server.domain.product.domain.Category;
 import kr.hhplus.be.server.domain.product.domain.Product;
@@ -25,6 +26,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
@@ -78,5 +80,26 @@ class PaymentServiceTest {
         assertThat(savedPayment.getMethod()).isEqualTo(PaymentMethod.POINT_PAYMENT);
         assertThat(savedPayment.getAmount()).isEqualTo(totalPrice);
         assertThat(savedPayment.getStatus()).isEqualTo(PaymentStatus.CONFIRMED);
+    }
+
+    @DisplayName("Payment - 실패 - 포인트 잔액이 부족할 경우 예외 발생")
+    @Test
+    void pay_Fail_InsufficientBalance() {
+        // given
+        User user = userRepository.save(UserFixture.USER());
+        Category category = categoryRepository.save(CategoryFixture.create("상의"));
+        Product product = productRepository.save(new Product("라넌큘러스 오버핏 맨투맨", category, 10_000, 10));
+
+        int quantity = 2;
+        Order order = new Order(user);
+        order.addOrderProduct(product, quantity);
+        orderRepository.save(order);
+
+        int initialBalance = 10_000;
+        pointRepository.save(new Point(user, initialBalance));
+
+        // when & then
+        assertThatThrownBy(() -> paymentService.pay(order, PaymentMethod.POINT_PAYMENT, order.calculateTotalPrice()))
+                .isInstanceOf(InsufficientPointBalanceException.class);
     }
 }
